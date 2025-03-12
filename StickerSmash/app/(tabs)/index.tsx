62eb3810898bb0134fd14707f54bd0a5ba2a8533
itsquelc <raquelnunescirino@gmail.com>
2025-03-12 10:49:39 -0300

    v dvdfgd

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/assets/components/ImageViewer";
+import Button from "@/assets/components/Button";
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -8,6 +9,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Escolher uma foto" theme="primary"/>
+        <Button label="Usar uma foto"/>
       </View>
     </View>
   );
@@ -23,4 +28,9 @@ export default function Index() {
     imageContainer: {
       flex: 1,
     }, 
+    footerContainer: {
+      flex: 1 / 3,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
   })
